74cdf4d35036858c8dbf688da8e7ec683e691edf
joeruiz <joseph.ruiz@quinnipiac.edu>
2019-01-28 17:02:33 -0500

    LS02calculator

---
+++ app/src/main/java/edu/quinnipiac/ser210/ls02calculator/CalcActivity.java
@@ -0,0 +1,13 @@
+package edu.quinnipiac.ser210.ls02calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
